472d2e172f8a30b92d4e477aa7b2fa6be51328b4
Gorka Aldasoro <gor.finidi@gmail.com>
2023-04-02 20:54:14 +0200

    new! hola mundo en python

---
+++ hello.py
@@ -0,0 +1,15 @@
+# Esto es un comentario: 
+# Nuestro "Hola Mundo" en Python
+print("Hola Python") # con comilla doble 
+print('Hola Python') # con comilla simple
+
+# Para consutar el tipo de dato de una variable
+print(type("Soy un dato str")) # str
+print(type(1)) # int
+print(type(1.0)) # float
+print(type(True)) # bool
+print(type([1,2,3])) # list
+print(type((1,2,3))) # tuple
+print(type({1,2,3})) # set
+print(type({'a':1,'b':2})) # dict
+print(type(3+1j)) # complex
