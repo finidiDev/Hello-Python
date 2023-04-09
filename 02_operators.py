db57db9271e269088d0cede1fcc68a7862769446
Gorka Aldasoro <gor.finidi@gmail.com>
2023-04-09 20:57:21 +0200

    new! operators

--- 02_operators.py
+++ 02_operators.py
@@ -1,4 +1,4 @@
-# Operadores
+# Operadores aritméticos
 print(3+4) # Suma
 print(3-4) # Resta
 print(3*4) # Multiplicación
@@ -6,3 +6,27 @@ print(3/4) # División
 print(3//4) # División entera
 print(3%4) # Módulo
 print(3**4) # Potencia
+
+# Operadores de asignación
+print("Hola" + " " + "Mundo") # Concatenación
+print("Hola" * 3) # Repetición
+print("Hola" + str(5)) # Conversión de tipos
+
+# Operadores de comparación
+print(3 == 4) # Igualdad
+print(3 != 4) # Desigualdad
+print(3 < 4) # Menor que
+print(3 > 4) # Mayor que
+print(3 <= 4) # Menor o igual que
+print(3 >= 4) # Mayor o igual que
+print("Hola" == "Hola") # Igualdad de cadenas, por ordenación alfabética
+print("Hola" != "Hola") # Desigualdad de cadenas, por ordenación alfabética
+print("Hola" < "Hola") # Menor que de cadenas, por ordenación alfabética
+print("Hola" > "Hola") # Mayor que de cadenas, por ordenación alfabética
+print("Hola" <= "Hola") # Menor o igual que de cadenas, por ordenación alfabética
+print("Hola" >= "Hola") # Mayor o igual que de cadenas, por ordenación alfabética
+
+# Operadores lógicos
+print(3 == 4 and 3 < 4) # AND
+print(3 == 4 or 3 < 4) # OR
+print(not 3 == 4) # NOT
